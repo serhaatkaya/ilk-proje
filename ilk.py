9300e3455e562a5972e257aa87e9f25ff55480d0
Serhat Kaya <44121239+serhaatkaya@users.noreply.github.com>
2018-10-14 00:07:37 +0300

    ilk-kodlar

---
+++ ilk.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    print('merhaba ',13/6,round(13/6))
+    x = 194
+    if x % 2 == 1:
+        # indented four spaces
+        print("{} tektir.".format(x))
+    else:
+        print("{} cifttir.".format(x))
